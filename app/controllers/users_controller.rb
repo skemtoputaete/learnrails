a34b1deadaf0e8136a024b1aa066ae543bf0f037
skemtoputaete <mak.goli@yandex.ru>
2017-06-29 13:33:46 +0300

    fix

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,7 +15,8 @@ class UsersController < ApplicationController
   end
   
   def show
-    @user = User.find(params[:id])  
+    @user = User.find(params[:id])
+	@articles = @user.articles
   end
 
   private
